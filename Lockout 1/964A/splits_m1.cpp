8549ff2daa9434a284d7010426aeb26242a1dbbd
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2022-09-02 20:54:17 +0600

    Create splits_m1.cpp

---
+++ Lockout 1/964A/splits_m1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#define pb push_back
+using namespace std;
+using ll = long long int;
+void fileio() {
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+}
+void solve() {
+}
+int main() {
+	// fileio();
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	ll num;
+	cin >> num;
+	cout << (num / 2 + 1) << endl;
+}
